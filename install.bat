2cddd9f3351aae0d4cf142d215919a380557f4f3
Holger Frydrych <holger@frydrych.org>
2023-07-14 22:19:31 +0200

    Add scripts to collect and install mod files

---
+++ install.bat
@@ -0,0 +1,5 @@
+set DIR=%~dp0
+call make_dist.bat
+
+xcopy /e /i /y %DIR%\dist\ %FARCRY_INSTALL_DIR%
+pause
---
+++ make_dist.bat
@@ -0,0 +1,22 @@
+set DIR=%~dp0
+set DIST=%DIR%dist
+if exist %DIST% (rmdir /S /Q %DIST%)
+mkdir %DIST%
+mkdir %DIST%\Bin32
+mkdir %DIST%\Mods
+mkdir %DIST%\Mods\CryVR
+mkdir %DIST%\Mods\CryVR\Bin32
+
+cd %DIR%\FCData
+7z a CryVR.zip .\*
+
+copy %DIR%\ModDesc.txt %DIST%\Mods\CryVR
+copy %DIR%\FarCryVR.bat %DIST%
+copy %DIR%\README.md %DIST%\Mods\CryVR
+copy %DIR%\LICENSE.md %DIST%\Mods\CryVR
+copy %DIR%\EULA.txt %DIST%\Mods\CryVR
+copy %DIR%\Sources\ThirdParty\openvr\bin\win32\openvr_api.dll %DIST%\Bin32\
+copy %DIR%\Sources\ThirdParty\dxvk\bin\d3d9.dll %DIST%\Bin32\d3d9.vr.dll
+copy "%DIR%\Sources\CryGame C++\Solution1\CryGame\Release\CryGame.dll" %DIST%\Mods\CryVR\Bin32\
+copy %DIR%\FCData\CryVR.zip %DIST%\Mods\CryVR\CryVR.pak
+erase %DIR%\FCData\CryVR.zip
