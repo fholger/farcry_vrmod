008f6b83317885c992ffb00c03cacbe0e8164042
Holger Frydrych <holger@frydrych.org>
2023-08-13 13:51:27 +0200

    Disable dxvk_async for now as it appears to cause crashes on my AMD system

--- FarCryVR.bat
+++ FarCryVR.bat
@@ -1,8 +1,8 @@
 cd bin32
 copy /Y d3d9.vr.dll d3d9.dll
 
-set DXVK_ASYNC=1
-set DXVK_GPLASYNCCACHE=1
+set DXVK_ASYNC=0
+set DXVK_GPLASYNCCACHE=0
 start /B /WAIT FarCry.exe "-MOD:CryVR"
 
 erase d3d9.dll
