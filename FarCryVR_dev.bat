32364a63f0321d34089ae05fabefda7101f9f1f5
Holger Frydrych <holger@frydrych.org>
2023-08-22 20:58:55 +0200

    Remove d3d9 dll renaming as it does not work for the Ubisoft version

--- FarCryVR_dev.bat
+++ FarCryVR_dev.bat
@@ -1,9 +1,6 @@
 cd bin32
-copy /Y d3d9.vr.dll d3d9.dll
 
 set DXVK_ASYNC=0
 set DXVK_GPLASYNCCACHE=0
 set DXVK_STARTOPENVR=1
 start /B /WAIT FarCry.exe "-MOD:CryVR" -DEVMODE
-
-erase d3d9.dll
--- make_dist.bat
+++ make_dist.bat
@@ -23,7 +23,7 @@ copy %DIR%\README.md %DIST%\Mods\CryVR
 copy %DIR%\LICENSE.md %DIST%\Mods\CryVR
 copy %DIR%\EULA.txt %DIST%\Mods\CryVR
 copy %DIR%\Sources\ThirdParty\openvr\bin\win32\openvr_api.dll %DIST%\Bin32\
-copy %DIR%\Sources\ThirdParty\dxvk\bin\d3d9.dll %DIST%\Bin32\d3d9.vr.dll
+copy %DIR%\Sources\ThirdParty\dxvk\bin\d3d9.dll %DIST%\Bin32\
 copy %DIR%\Sources\ThirdParty\ffmpeg\bin\*.dll %DIST%\Bin32\
 copy "%DIR%\Sources\CryGame C++\Solution1\CryGame\Release\CryGame.dll" %DIST%\Mods\CryVR\Bin32\
 copy %DIR%\FCData\CryVR.zip %DIST%\Mods\CryVR\CryVR.pak
